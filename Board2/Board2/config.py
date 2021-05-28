bcfa081f6b1f830c3fa6ef56cdb650e2f05d9160
alicesprings1 <aliceesprings@outlook.com>
2021-05-28 15:02:57 +0800

    去掉敏感信息

--- Board2/Board2/config.py
+++ Board2/Board2/config.py
@@ -1,9 +1,9 @@
 class Config:
         
     def __init__(self,client_id,**kw):
-        self.user='admin'
-        self.password='admin'
-        self.server='120.77.223.118'
+        self.user='username'
+        self.password='password'
+        self.server='mqtt_server_name'
         self.port=1883
         self.client=None
         self.client_id=client_id
